0685aa7c25cd72ac256cc802765bc5f3beb6a539
mrkaspa <michel.ingesoft@gmail.com>
2017-01-27 16:25:10 -0500

    wip

--- src/Main.elm
+++ src/Main.elm
@@ -7,63 +7,81 @@ import Time
 import View
 
 
-type alias Flags = {
-  swapCount : Int
-}
+type alias Flags =
+    { swapCount : Int
+    }
+
 
 main : Program Flags Model Msg
 main =
-  programWithFlags
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    programWithFlags
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
 
 -- MODEL
 
 
 type alias Model =
-  { count : Int
-  , swapCount: Int -- this will be updated by elm-hot callback. See index.js
-  , elapsed : Int
-  , alertText : String
-  , logs : List String
-  }
+    { count : Int
+    , swapCount :
+        Int
+        -- this will be updated by elm-hot callback. See index.js
+    , elapsed : Int
+    , alertText : String
+    , logs : List String
+    }
 
-init : Flags -> (Model, Cmd msg)
+
+init : Flags -> ( Model, Cmd msg )
 init flags =
-  (Model 0 0 0 "It works!" [], Cmd.none)
+    ( Model 0 0 0 "It works!" [], Cmd.none )
+
 
 
 -- UPDATE
 
 
-type Msg = Increment | Decrement | Tick | Alert | ChangeAlertText String | Log String
+type Msg
+    = Increment
+    | Decrement
+    | Tick
+    | Alert
+    | ChangeAlertText String
+    | Log String
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Increment ->
-      ({ model | count = model.count + 1 }, Cmd.none)
+    case msg of
+        Increment ->
+            ( { model | count = model.count + 1 }, Cmd.none )
 
-    Decrement ->
-      ({ model | count = model.count - 1 }, Cmd.none)
+        Decrement ->
+            ( { model | count = model.count - 1 }, Cmd.none )
 
-    Tick ->
-      ({ model | elapsed = model.elapsed + 1}, Cmd.none)
+        Tick ->
+            ( { model | elapsed = model.elapsed + 1 }, Cmd.none )
 
-    Alert ->
-      (model, alert model.alertText)
+        Alert ->
+            ( model, alert model.alertText )
 
-    ChangeAlertText text ->
-      ({ model | alertText = text }, Cmd.none)
+        ChangeAlertText text ->
+            ( { model | alertText = text }, Cmd.none )
 
-    Log text ->
-      ({ model | logs = text :: model.logs }, Cmd.none)
+        Log text ->
+            ( { model | logs = text :: model.logs }, Cmd.none )
+
 
 port alert : String -> Cmd msg
+
+
 port log : (String -> msg) -> Sub msg
+
 
 
 -- SUBSCRIPTIONS
@@ -71,47 +89,50 @@ port log : (String -> msg) -> Sub msg
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.batch
-    [ Time.every Time.second <| always Tick
-    , log Log
-    ]
+    Sub.batch
+        [ Time.every Time.second <| always Tick
+        , log Log
+        ]
+
 
 
 -- VIEW
 
+
 boxStyle : List ( String, String )
 boxStyle =
-  [ ("border", "1px solid #ccc")
-  , ("border-radius", "4px")
-  , ("padding", "10px")
-  , ("margin", "10px")
-  ]
+    [ ( "border", "1px solid #ccc" )
+    , ( "border-radius", "4px" )
+    , ( "padding", "10px" )
+    , ( "margin", "10px" )
+    ]
+
 
 view : Model -> Html.Html Msg
 view model =
-  div []
-    [ div [ style boxStyle ]
-        [ text <| "This app has been hot-swapped " ++ (toString model.swapCount) ++ " times." ]
-    , div [ style boxStyle ]
-        [ View.view ]
-    , div [ style boxStyle ]
-        [ h2 [] [ text "Counter" ]
-        , button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (toString model.count) ]
-        , button [ onClick Increment ] [ text "+" ]
+    div []
+        [ div [ style boxStyle ]
+            [ text <| "This app has been hot-swapped " ++ (toString model.swapCount) ++ " times." ]
+        , div [ style boxStyle ]
+            [ View.view ]
+        , div [ style boxStyle ]
+            [ h2 [] [ text "Counter" ]
+            , button [ onClick Decrement ] [ text "-" ]
+            , div [] [ text (toString model.count) ]
+            , button [ onClick Increment ] [ text "+" ]
+            ]
+        , div [ style boxStyle ]
+            [ h2 [] [ text "Time.every second" ]
+            , text <| "elapsed seconds: " ++ (toString model.elapsed)
+            ]
+        , div [ style boxStyle ]
+            [ h2 [] [ text "Outgoing Port" ]
+            , input [ type_ "text", value model.alertText, onInput ChangeAlertText ] []
+            , button [ onClick Alert ] [ text "call alert" ]
+            ]
+        , div [ style boxStyle ]
+            [ h2 [] [ text "Incoming Port" ]
+            , ul [] <|
+                List.map (\t -> li [] [ text t ]) model.logs
+            ]
         ]
-    , div [ style boxStyle ]
-        [ h2 [] [ text "Time.every second" ]
-        , text <| "elapsed seconds: " ++ (toString model.elapsed)
-        ]
-    , div [ style boxStyle ]
-        [ h2 [] [ text "Outgoing Port" ]
-        , input [ type_ "text", value model.alertText, onInput ChangeAlertText ] []
-        , button [ onClick Alert ] [ text "call alert" ]
-        ]
-    , div [ style boxStyle ]
-        [ h2 [] [ text "Incoming Port" ]
-        , ul [] <|
-            List.map (\t -> li [] [ text t]) model.logs
-        ]
-    ]
